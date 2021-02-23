6b688bd5ce4d0756318848b0e31dd6f6454945d9
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-02-23 11:45:17 -0700

    update comments

--- project1.c
+++ project1.c
@@ -1,3 +1,19 @@
+/*
+	File: project1.c - Implements the collatz conjecture process in C using a pipe & fork
+	
+	This file implements the collatz conjecture using a linux pipe & fork,
+	as well as unsigned long longs for numbers to ensure a large range.
+	Takes a single, positive argument when running the executable, which
+	is then run through the process with all numbers being passed through a pipe
+	and printed.
+
+	Author: Quinn Kleinfelter
+	Class: EECS 3540-001 Operating Systems & Systems Programming Spring 2021
+	Instructor: Dr. Thomas
+	Last Edited: 2/23/21
+	Copyright: Copyright 2021 by Quinn Kleinfelter. All rights reserved.
+*/
+
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -23,8 +39,8 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	errno = 0;
-	char* endPtr;
+	errno = 0; // Clear errno just in case
+	char* endPtr; // pointer for strtoull's second argument
 	unsigned long long num = strtoull(argv[1], &endPtr, 10);
 
 	if (num == 0 || *endPtr) {
@@ -35,6 +51,8 @@ int main(int argc, char* argv[]) {
 	}
 
 	if (num == ULLONG_MAX && errno == ERANGE) {
+		// If we have ULLONG_MAX in num & errno shows ERANGE then that means
+		// we overflowed when parsing, i.e. 18446744073709551616 or higher was the input
 		fprintf(stderr, "Number input was too large [ARG_OVERFLOW]\n");
 		return 1;
 	}
@@ -57,11 +75,14 @@ int main(int argc, char* argv[]) {
 		return 1;
 	} else if (pid == 0) {
 		// Here we're the child process
-		close(fd[READ_END]); // won't be reading here 	
+		close(fd[READ_END]); // and thus we won't be reading
+
 		sprintf(write_msg, "%llu", num); // put our starting digit into write_msg, formatted as a long
-		write(fd[WRITE_END], write_msg, strlen(write_msg) + 1); // write the first num to the pipe
+		write(fd[WRITE_END], write_msg, strlen(write_msg) + 1); // write the first number to the pipe
+
 		unsigned long long s = 0; // keeps track of number of steps we make
 		unsigned long long h = num; // largest number, we can start with the first number
+
 		while (num != 1) {
 			// If num isn't 1, continuously loop either dividing by 2 or multiplying by 3 and adding 1, thus running the collatz conjecture
 			if (num % 2 == 0)
@@ -69,39 +90,47 @@ int main(int argc, char* argv[]) {
 			else 
 				num = (num * 3) + 1;
 
-			s++;
-			if (num > h)
+			s++; // increment the number of steps
+			if (num > h) // and check if this is our new largest number
 				h = num;
-			// Writing the next number to the pipe
+
+			// Write the next number to the pipe
 			sprintf(write_msg, "%llu", num);
 			write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
 		}
+
+		// Now that we are out of the loop, we need to write s and h to the pipe
+		// so they can be printed as well
 		sprintf(write_msg, "%llu", s);
 		write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
 
 		sprintf(write_msg, "%llu", h);
 		write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
 
-		close(fd[WRITE_END]); // Close the pipe, will send EOF to the read end
+		close(fd[WRITE_END]); // Close the pipe, which will send EOF to the read end
 	} else {
 		// Parent process
-		close(fd[WRITE_END]); // won't be writing here
+		close(fd[WRITE_END]); // thus we won't be writing here
 
 		// read returns > 0 if we aren't at EOF, so this will loop until we close the pipe on the other end 
 		while (read(fd[READ_END], read_msg, BUFFER_SIZE) > 0) {
 			printf("%s", read_msg); // read in and print out the digit given to us
+
 			if (strcmp(read_msg, "1") != 0) // if the number isn't 1, print a comma and a space
 				printf(", ");
-			else // otherwise, break out of the loop so we can handle the last 2 things
+			else // otherwise, break out of the loop so we can handle printing s & h
 				break;
 		}
+		// Now that we're out of the loop, print out s & h formatted correctly
 		read(fd[READ_END], read_msg, BUFFER_SIZE);
 		printf(" (%s, ", read_msg);
+
 		read(fd[READ_END], read_msg, BUFFER_SIZE);
 		printf("%s)\n", read_msg);
 
 		close(fd[READ_END]); // close the read end of the pipe
-		wait(NULL);
+		wait(NULL); // wait for the child process, just in case we have something weird happening
 	}
+	
 	return 0;
 }
