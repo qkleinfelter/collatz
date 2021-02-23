f6aa7248787724f433609b7bf7fdbd360d967a5f
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-02-23 11:34:00 -0700

    refactor the math into the child

--- project1.c
+++ project1.c
@@ -60,6 +60,8 @@ int main(int argc, char* argv[]) {
 		close(fd[READ_END]); // won't be reading here 	
 		sprintf(write_msg, "%llu", num); // put our starting digit into write_msg, formatted as a long
 		write(fd[WRITE_END], write_msg, strlen(write_msg) + 1); // write the first num to the pipe
+		unsigned long long s = 0; // keeps track of number of steps we make
+		unsigned long long h = num; // largest number, we can start with the first number
 		while (num != 1) {
 			// If num isn't 1, continuously loop either dividing by 2 or multiplying by 3 and adding 1, thus running the collatz conjecture
 			if (num % 2 == 0)
@@ -67,32 +69,39 @@ int main(int argc, char* argv[]) {
 			else 
 				num = (num * 3) + 1;
 
+			s++;
+			if (num > h)
+				h = num;
 			// Writing the next number to the pipe
 			sprintf(write_msg, "%llu", num);
 			write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
-		} 
+		}
+		sprintf(write_msg, "%llu", s);
+		write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
+
+		sprintf(write_msg, "%llu", h);
+		write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
+
 		close(fd[WRITE_END]); // Close the pipe, will send EOF to the read end
 	} else {
 		// Parent process
 		close(fd[WRITE_END]); // won't be writing here
-		unsigned long long s = -1; // keeps track of number of steps we make, start at -1 since the initial number comes through the pipe as well
-		unsigned long long h = 0; // largest number, starting with is fine since all numbers in a collatz conjecture are >= 0
 
 		// read returns > 0 if we aren't at EOF, so this will loop until we close the pipe on the other end 
 		while (read(fd[READ_END], read_msg, BUFFER_SIZE) > 0) {
-			unsigned long long read_num = strtoull(read_msg, NULL, 10);
-			printf("%llu", read_num); // read in and print out the digit given to us
-			if (read_num != 1) // if the number isn't 1, print a comma and a space
+			printf("%s", read_msg); // read in and print out the digit given to us
+			if (strcmp(read_msg, "1") != 0) // if the number isn't 1, print a comma and a space
 				printf(", ");
-			else // otherwise, just a space
-				printf(" ");
-			s++;
-			if (read_num > h) // check if our new number is larger than the largest
-				h = read_num;
+			else // otherwise, break out of the loop so we can handle the last 2 things
+				break;
 		}
+		read(fd[READ_END], read_msg, BUFFER_SIZE);
+		printf(" (%s, ", read_msg);
+		read(fd[READ_END], read_msg, BUFFER_SIZE);
+		printf("%s)\n", read_msg);
+
 		close(fd[READ_END]); // close the read end of the pipe
 		wait(NULL);
-		printf("(%llu, %llu)\n", s, h); // and output our final statistics
 	}
 	return 0;
 }
